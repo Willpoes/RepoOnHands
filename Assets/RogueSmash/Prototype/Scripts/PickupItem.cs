cbea8ae93bcceac1c4a1a1c42666a9de42fd3523
WillPoes <wilerutap@gmail.com>
2025-03-04 10:06:08 -0500

    Updating character controller, proejctile, pickup and weaponcontroller

--- Assets/RogueSmash/Prototype/Scripts/PickupItem.cs
+++ Assets/RogueSmash/Prototype/Scripts/PickupItem.cs
@@ -6,32 +6,20 @@ namespace MyCompany.RogueSmash.Prototype
     [RequireComponent(typeof(BoxCollider))]
     public class PickupItem : MonoBehaviour
     {
-        /// <summary>
-        /// Callback to be invoked when this
-        /// a trigger event is fired by Unity's collision system.
-        /// </summary>
         private Action<GameObject> onPickedUp;
 
-        /// <summary>
-        /// Because we can't use a constructor in a
-        /// monobehaviour, we instead provide a manually-called
-        /// Init method.
-        /// </summary>
-        /// <param name="onPickedUp">Callback to be invoked on trigger event</param>
         public void Init(Action<GameObject> onPickedUp)
         {
             this.onPickedUp = onPickedUp;
         }
-    
+
         private void OnTriggerEnter(Collider other)
         {
-            if (other != null)
+            if (other.CompareTag("Player"))
             {
-                Debug.Log("ENTERED");
-                if (onPickedUp != null)
-                {
-                    onPickedUp.Invoke(other.gameObject);
-                }
+                Debug.Log($"{this.gameObject.name} collected by {other.gameObject.name}");
+                onPickedUp?.Invoke(other.gameObject);
+                Destroy(gameObject);
             }
         }
     }
